30225059b54c270b998e93f033f98a3d754ccfc6
Bryan <89350478+sabahbryan@users.noreply.github.com>
2026-02-05 00:04:05 +0800

    Edited1
    Added copy array. Still need to tidy up.

--- module5/ex02/play_with_arrays.py
+++ module5/ex02/play_with_arrays.py
@@ -4,16 +4,29 @@
 
 arr = [2, 8, 9, 48, 8, 22, -12, 2]
 
+arr2 = arr.copy()
+
 i = 0
 
 print(arr)
 
 while i < len(arr):
     arr[i] += 2
-    print("A")
+#    print("A")
+#    if arr[i] <= 5:
+#        arr.pop(i)
+    i += 1
+
+print(arr)
+
+i = 0
+
+while i < len(arr):
+#    print("B")
     if arr[i] <= 5:
-        print("B")
+#        print("C")
         arr.pop(i)
+        i = 0
     i += 1
 
 print(arr)
