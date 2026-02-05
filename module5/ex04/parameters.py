7eb17c66fd9ba9bb791ed1070bc230e452e88ec3
Bryan <89350478+sabahbryan@users.noreply.github.com>
2026-02-05 20:39:32 +0800

    Remove redundant code
    Commented out redundant code.

--- module5/ex04/parameters.py
+++ module5/ex04/parameters.py
@@ -4,11 +4,11 @@ import sys
 
 #print(sys.argv)
 
-# start at '1' to ignore "parameters.py"
-i = 1
+# start at '1' to ignore "parameters.py" when "print()"
+#i = 1
 
-while i < len(sys.argv):
+#while i < len(sys.argv):
     #print(sys.argv[i])
-    i += 1
+#    i += 1
 
 print("Number of parameters:", len(sys.argv) - 1)
